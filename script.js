b420079a854134a02b134d516ea60e9b97f16ee8
Mateusfarofa <162995629+Mateusfarofa@users.noreply.github.com>
2024-11-27 19:38:34 -0300

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-var imgagora = "corte2.jpg";
-var imgdepois = "corte1.jpg";
-
-function troca(){
-  document.getElementById("corte").src = imgagora;
-   let aux = "imgagora";
-  "imgagora" = "imgdepois";
-  "imgdepois" = aux;
-}

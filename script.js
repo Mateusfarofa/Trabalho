0cacaf749bc152c613b9c20ebaccfaf9a5a2a7d9
Mateusfarofa <162995629+Mateusfarofa@users.noreply.github.com>
2024-11-27 19:14:47 -0300

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var imgagora = "corte2.jpg";
 var imgdepois = "corte1.jpg";
 
 function trocar(){
-  document.getElementById("corte").src = imgagora;
+  document.getElementById("troca()").src = imgagora;
    let aux = imgagora;
   imgagora = imgdepois;
   imgdepois = aux;
